06451a0004a2a1d327d4124d0c37dccf0f881a85
softhightech <contact@softhightech.com>
2017-07-01 14:14:35 +0000

    Menu update

--- includes/public/menu/menu.php
+++ includes/public/menu/menu.php
@@ -15,12 +15,8 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li>
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">General <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="<?php echo WebSite;?>Users">Users</a></li>
-              <li><a href="<?php echo WebSite;?>Logout">Logout</a></li>
-            </ul>
-
+        <li><a href="<?php echo WebSite;?>Users">Users</a></li>
+        <li><a href="<?php echo WebSite;?>Logout">Logout</a></li>
         </li>
       </ul>
     </div><!-- /.navbar-collapse -->
