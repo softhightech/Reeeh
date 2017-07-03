dd23f99af950cfd98c15577fd58e0d2a009f5f35
softhightech <contact@softhightech.com>
2017-07-03 11:44:56 +0100

    i changed class users.class.php i removed and updated some methodes

--- includes/classes/users/users.class.php
+++ includes/classes/users/users.class.php
@@ -44,7 +44,7 @@ class user
 	public static function getByID($user)
 	{
             $DB = Database::getInstance();
-            $query = "SELECT * FROM users WHERE id = $user";
+            $query = "SELECT * FROM users WHERE id_user = $user";
             
             if($res = $DB->query($query))
             {
@@ -59,7 +59,7 @@ class user
     public static function getEmailByID($user)
     {
             $DB = Database::getInstance();
-            $query = "SELECT email FROM users WHERE id = $user";
+            $query = "SELECT email FROM users WHERE id_user = $user";
             if($res = $DB->query($query))
             {
                 if($res->rowCount() == 1)
@@ -101,7 +101,7 @@ class user
             $password = md5($password);
             try
             {
-                $query = "SELECT id FROM users WHERE email = '$email' AND password = '$password'";
+                $query = "SELECT id_user as id FROM users WHERE email = '$email' AND password = '$password'";
                 if($res = $DB->query($query))
                 {
                     if($res->rowCount() > 0)
@@ -124,7 +124,7 @@ class user
 	{
 		try
 		{
-			if($res = $GLOBALS['DB']->query("SELECT * FROM user WHERE ".$condition." ;"))
+			if($res = $GLOBALS['DB']->query("SELECT id_user as id,* FROM user WHERE ".$condition." ;"))
 			{
 				if($res->rowCount() >= 1)
 				{
@@ -249,16 +249,6 @@ class user
 			return 0;
 		}
 	}
-	/*
-	public static function connected() {
-		
-		if ( isset($_SESSION['user']) ) {
-			return $_SESSION['user'];
-		} else {
-			return false;
-		}
-	}
-    */
 	
 	public function full_name() {
 		return strtoupper($this->m_firstname).' '.strtoupper($this->m_lastname);
@@ -271,295 +261,5 @@ class user
             return true;
             
 	}
-        /*****************************/
-        
-        public static function getUserJob($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT jobs.name as name FROM users_jobs join jobs on (jobs.id = users_jobs.id_job) WHERE users_jobs.id_user=$user and users_jobs.deleted = 0;";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['name'] != null)
-                {
-                    return $result['name'];
-                }else{
-                    return '';
-                }
-            }
-        }
-        public static function getUserJobId($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT jobs.id FROM users_jobs join jobs on (jobs.id = users_jobs.id_job) WHERE users_jobs.id_user=$user and users_jobs.deleted = 0;";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['id'] != null)
-                {
-                    return $result['id'];
-                }else{
-                    return '';
-                }
-            }
-        }
-
-
-        public static function getUserEducation($user)
-        {
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "select id,name from educations where id =(SELECT id_education from users_educations where id_user=$user order by id desc limit 1)";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['name'] != null)
-                {
-                    return $result['name'];
-                }else{
-                    return '';
-                }
-            }
-        }
-
-        public static function getUserEducationId($user)
-        {
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "select id from educations where id =(SELECT id_education from users_educations where id_user=$user order by id desc limit 1)";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['id'] != null)
-                {
-                    return $result['id'];
-                }else{
-                    return '';
-                }
-            }
-        }
-
-        public static function getUserCity($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT cities.name as name FROM cities join users_cities on (cities.id = users_cities.id_city) WHERE users_cities.id_user=$user and users_cities.deleted=0 order by users_cities.id desc ";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['name'] != null)
-                {
-                    return $result['name'];
-                }else{
-                    return '';
-                }
-            }
-        }
-        public static function getUserCityId($user){
-            /*
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT cities.id FROM cities join users_cities on (cities.id = users_cities.id_city) WHERE users_cities.id_user=$user and users_cities.deleted=0 order by users_cities.id desc";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['id'] != null)
-                {
-                    return $result['id'];
-                }else{
-                    return '';
-                }
-            }
-            */
-        }
-
-
-        public static function getUserCountry($user)
-        {
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT countries.name as name,countries.id as id FROM countries join users_countries on (countries.id = users_countries.id_country) WHERE users_countries.id_user=$user  order by users_countries.id desc";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['name'] != null)
-                {
-                    return $result['name'];
-                }else{
-                    return '';
-                }
-            }
-        }
-        public static function getUserCountryId($user)
-        {
-            /*
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT countries.id FROM countries join users_countries on (countries.id = users_countries.id_country) WHERE users_countries.id_user=$user  order by users_countries.id desc";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['id'] != null)
-                {
-                    return $result['id'];
-                }else{
-                    return '';
-                }
-            }
-            */
-        }
-
-        public static function getUserSubscription($user){
-            return '2013-20-21';
-        }
-        public static function getDefaultPicture($user){
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $DB = Database::getInstance();
-                $sql = "select link from profile_pictures where id_user =$user and `default` = 1";
-//                echo $sql;
-                $query3=$DB->query($sql);
-                $ImageRES=$query3->fetch(PDO::FETCH_ASSOC);
-                if(file_exists($ImageRES['link']))
-                {
-                    return $ImageRES['link'];
-                }else{
-                    return 'images/avatar/default.png';
-                }
-            }
-        }
-        public static function getUserCityCountry($user){
-            if(self::getUserCountry($user) != '')
-            {
-                return self::getUserCity($user).', '.self::getUserCountry($user);       
-            }else{
-                return self::getUserCity($user);  
-            }
-        }
-        public static function getUserFirstLastName($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT first_name as fname,last_name as lname FROM users where id = $user";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['fname'] != null)
-                {
-                    return ucfirst($result['fname']).' '.ucfirst($result['lname']);
-                }else{
-                    return '';
-                }
-            }
-        }
-        public static function getUserFirstName($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT first_name as name FROM users where id = $user";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['name'] != null)
-                {
-                    return $result['name'];
-                }else{
-                    return '';
-                }
-            }
-        }
-        public static function getUserLastName($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT username as name FROM users where id = $user";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['name'] != null)
-                {
-                    return $result['name'];
-                }else{
-                    return '';
-                }
-            }
-        }
-
-
-        public static function getUserType($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT type FROM users where id = $user";
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['type'] != null)
-                {
-                    return $result['type'];
-                }else{
-                    return '';
-                }
-            }
-        }
-
-        public static function dateOfSubscription($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT cdate as date FROM users where id = $user";
-                //echo $sql;
-                $query = $DB->query($sql);
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                if($result['date'] != null)
-                {
-                    return $result['date'];
-                }else{
-                    return '';
-                }
-            }
-        }
-        // verify if the user is new
-        public static function newUser($user){
-            $DB = Database::getInstance();
-            $Security = new Security();
-            if($Security->check_numbers($user))
-            {
-                $sql = "SELECT id FROM users where DATE(cdate) = DATE(NOW()) and id = $user";
-                //echo $sql;
-                if($res = $DB->query($sql))
-                {
-                    if($res->rowCount() == 1)
-                    {
-                        return true;
-                    }else{
-                        return false;
-                    }
-                }else{
-                    return false;
-                }
-            }
-        }
-
 }
 ?>
